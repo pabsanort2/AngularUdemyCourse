968dafd3d2e44df48a99ec8fd99388f2d0855647
pabsanort2 <pabsanort2@alum.us.es>
2024-03-26 12:25:29 +0100

    Added functionality to the create product form
    For now the form just logs the form values in the console

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-crear-producto',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './crear-producto.component.html',
-  styleUrl: './crear-producto.component.css'
+  styleUrl: './crear-producto.component.css',
 })
 export class CrearProductoComponent {
+  productoForm = new FormGroup({
+    producto: new FormControl('', Validators.required),
+    categoria: new FormControl('', Validators.required),
+    ubicacion: new FormControl('', Validators.required),
+    precio: new FormControl('', Validators.required),
+  });
 
+  agregarProducto() {
+    console.log(this.productoForm);
+  }
 }
