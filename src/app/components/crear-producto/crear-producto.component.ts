885940567e0c0cf0122096d875618a3ad6337f16
pabsanort2 <pabsanort2@alum.us.es>
2024-03-27 11:22:23 +0100

    Added message when the user creates a product and also created the object for it
    Added a toastr notification when the user creates a product succesfully. I had to research for a solution for a standalone Angular app since the given solution was for module based apps.

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -7,7 +7,9 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { Producto } from '../../models/producto';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-crear-producto',
@@ -17,6 +19,7 @@ import { RouterModule } from '@angular/router';
   styleUrl: './crear-producto.component.css',
 })
 export class CrearProductoComponent {
+
   productoForm = new FormGroup({
     producto: new FormControl('', Validators.required),
     categoria: new FormControl('', Validators.required),
@@ -24,7 +27,21 @@ export class CrearProductoComponent {
     precio: new FormControl('', Validators.required),
   });
 
+  constructor(private router: Router, private toastr: ToastrService){}
+
   agregarProducto() {
-    console.log(this.productoForm);
+    // We create a product object
+    const PRODUCTO: Producto = {
+      // We need to use the ! operator that tells typescript that even though something looks like it could be null, it can trust you that it's not 
+      nombre: this.productoForm.get("producto")?.value!,
+      categoria: this.productoForm.get("categoria")?.value!,
+      ubicacion: this.productoForm.get("ubicacion")?.value!,
+      //In this case, price is a number, so we have to convert the input string to a number, simply using the + operator
+      precio: +this.productoForm.get('precio')?.value!,
+    }
+
+    console.log(PRODUCTO);
+    this.toastr.success('El producto fue registrado con exito', 'Producto registrado')
+    this.router.navigate(['/'])
   }
 }
