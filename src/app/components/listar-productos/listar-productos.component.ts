7d18506107cb2993719b5cb97ed86761e86d921e
pabsanort2 <pabsanort2@alum.us.es>
2024-03-29 12:49:35 +0100

    Linked the product listing with the html template

--- src/app/components/listar-productos/listar-productos.component.ts
+++ src/app/components/listar-productos/listar-productos.component.ts
@@ -2,16 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductoService } from '../../services/producto.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Producto } from '../../models/producto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listar-productos',
   standalone: true,
   // Had to import RouterModule to this child component since the routerLink was not working if it was not imported
-  imports: [RouterModule, HttpClientModule],
+  imports: [RouterModule, HttpClientModule, CommonModule],
   templateUrl: './listar-productos.component.html',
   styleUrl: './listar-productos.component.css',
 })
 export class ListarProductosComponent implements OnInit {
+
+  listProductos: Producto [] = [];
+
   constructor(private _productoService: ProductoService) {}
   ngOnInit(): void {
     this.obtenerProductos();
@@ -21,6 +26,7 @@ export class ListarProductosComponent implements OnInit {
     this._productoService.getProductos().subscribe(
       (data) => {
         console.log(data);
+        this.listProductos = data;
       },
       (error) => {
         console.log(error);
